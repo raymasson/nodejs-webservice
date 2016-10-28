f5b1eb2bb235970b3788baa11405b921ec14a1bb
Raymond MASSON <Raymond MASSON>
2016-10-28 11:38:30 +0200

    Get and Post

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var server = express();
 
 var port = process.env.PORT || 3000;
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+var bookRouter = require('./routes/bookRoutes')(Book);
+
+server.use('/api/books', bookRouter);
 
 server.get('/', function (req, res) {
     res.send('Welcome to my API!')
